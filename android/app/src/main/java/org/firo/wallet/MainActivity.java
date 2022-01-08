c6f5685a3603d8c2fbc7145f21b3792420e8dcc7
hzak <hayk.zakaryan@picsart.com>
2022-01-08 17:26:53 +0400

    change package name

--- android/app/src/main/java/org/firo/wallet/MainActivity.java
+++ android/app/src/main/java/org/firo/wallet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.firomobilewallet;
+package org.firo.wallet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "FiroMobileWallet";
+    return "FiroWallet";
   }
 }
--- android/app/src/main/java/org/firo/wallet/MainApplication.java
+++ android/app/src/main/java/org/firo/wallet/MainApplication.java
@@ -1,10 +1,9 @@
-package com.firomobilewallet;
+package org.firo.wallet;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -63,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.firomobilewallet.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("org.firo.wallet.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
