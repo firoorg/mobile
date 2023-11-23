c2ae8cfb88804e300605883d6503cf391f39d1e4
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2023-11-23 21:53:08 +0700

    enable indonesia

--- src/localization/languages.js
+++ src/localization/languages.js
@@ -6,4 +6,5 @@ type Language = {
 export const SupportedLanguages: Language[] = Object.freeze([
   {label: 'English', value: 'en'},
   {label: 'Chinese', value: 'zh'},
+  {label: 'Bahasa Indonesia', value: 'id'},
 ]);
